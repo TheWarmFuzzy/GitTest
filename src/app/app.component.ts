567288edb76635c6fa82160ca1b23db210c09517
bTOutlaw <morganejackman@gmail.com>
2018-04-18 17:40:06 -0400

    This is a test
    Test button
    Array function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Master';
+
+  myArray: string[];
+
+  constructor() { this.myArray=["Jeffrey is a decent teacher","This is going pretty well thus far","Hopefully I don't fuck this up too bad"]}
+
+  private _test() { console.log("Hello, world!") }
 }
